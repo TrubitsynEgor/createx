77f446560a683c7d828089de6e97be8d447295a5
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-02-01 17:29:20 +0500

    fix accessibility + semantics

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,7 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+
+const goTop = document.querySelector('.footer__copyright-btn');
+goTop.addEventListener('click', () => window.scroll({ top: 0, left: 0, behavior: 'smooth' }))
