ef1bbab42fb2322de582070e4d3f1cd431d20db9
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-02-03 21:10:19 +0500

    Доработка табов + связка с кнопкой loadMore

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,16 +1,55 @@
 const tabsNav = document.querySelector('.services-block__tabs');
+const tabBody = document.querySelectorAll('.work-content__item');
+const tabsBtn = document.querySelectorAll('.services-block__btn');
+const tabBodyActive = document.querySelectorAll('.work-content__item.active');
+const loadMoreBtn = document.querySelector('.load-more');
+
+const isLoadMoreNeeded = (selector) => {
+  if (selector.length <= 9) {
+    loadMoreBtn.style.display = 'none';
+  } else {
+    loadMoreBtn.style.display = 'block';
+  }
+}
 
 tabsNav.addEventListener('click', el => {
   const target = el.target;
   if (target.classList.contains('services-block__btn')) {
     const path = target.dataset.path;
-    document.querySelectorAll('.work-content__item').forEach(el => el.style.display = 'none');
-    document.querySelectorAll(`[data-target="${path}"]`).forEach(el => el.style.display = 'flex');
-    document.querySelectorAll('.services-block__btn').forEach(el => el.classList.remove('active'));
-    document.querySelector(`[data-path="${path}"]`).classList.add('active')
-    if (target.dataset.path == 'all') {
-      document.querySelectorAll('.work-content__item').forEach(el => el.style.display = 'flex');
+
+    tabBody.forEach(el => {
+      el.classList.remove('active');
+      el.classList.remove('active-more');
+    });
+    document.querySelectorAll(`[data-target="${path}"]`).forEach(el => el.classList.add('active'));
+
+    isLoadMoreNeeded(document.querySelectorAll(`[data-target="${path}"]`));
+
+    tabsBtn.forEach(el => el.classList.remove('active'));
+    target.classList.add('active')
+    if (path == 'all') {
+      tabBody.forEach(el => el.classList.add('active'));
+
+      isLoadMoreNeeded(document.querySelectorAll('.work-content__item.active'));
+
     }
   }
 })
 
+//todo ДОРАБОТАТЬ: Исчезновение элементов более 9 через JS (в сss - nth-child(n+10) работает не так как нужно)
+
+isLoadMoreNeeded(tabBodyActive);
+
+loadMoreBtn.addEventListener('click', (el) => {
+  const activeItems = document.querySelectorAll('.work-content__item.active');
+
+  const path = document.querySelector('.services-block__btn.active').dataset.path;
+  if (path == 'all') {
+    tabBody.forEach(el => {
+      el.classList.add('active-more');
+      loadMoreBtn.style.display = 'none';
+    })
+  } else {
+    document.querySelectorAll(`[data-target="${path}"]`).forEach(el => el.classList.add('active-more'));
+  }
+})
