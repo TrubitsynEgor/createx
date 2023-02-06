61a3c9c4304670619722c206b9f4fe613d0d499a
TrubitsynEgor <trubitsynwork74@gmail.com>
2023-02-06 19:14:33 +0500

    add page positions add section hero

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,9 @@ import './_components';
 
 const goTop = document.querySelector('.footer__copyright-btn');
 goTop.addEventListener('click', () => window.scroll({ top: 0, left: 0, behavior: 'smooth' }))
+
+import Choices from 'choices.js';
+const element = document.querySelector('.custom-select');
+const choices = new Choices(element, {
+  searchEnabled: false,
+});
